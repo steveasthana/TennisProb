93d573cefe6dac2cb259258e0b3767371e2e522c
Steve Asthana <steve.asthana@gmail.com>
2023-09-24 22:23:05 -0400

    Update App UI

---
+++ src.js
@@ -0,0 +1,18 @@
+import {math} from 'mathjs'
+
+var testButton = document.getElementById("testButton");
+button.addEventListener("click", testMath);
+
+function updatePlayers() {
+    var player1Name = document.getElementById("player1Name").value;
+    var player2Name = document.getElementById("player2Name").value;
+}
+
+function calculateProb(player1prob, player2prob) {
+    var player1prob = document.getElementById("player1prob").value;
+    var player2prob = document.getElementById("player2prob").value;
+}
+
+function testMath() {
+    console.log("please work");
+}
